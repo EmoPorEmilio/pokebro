9724049870ba935140af3540495f60329a9f4b1a
Emilio Franceschini <franceschini.emilio@gmail.com>
2022-03-11 21:48:54 -0300

    arreglo estado inicial

--- src/components/Options/index.js
+++ src/components/Options/index.js
@@ -2,8 +2,10 @@ import { OptionsContainer, Option, OptionLoader } from './Options.styles';
 
 export const Options = ({loading, pokemonNameOptions, handleClickOption}) => {
     return <OptionsContainer>
-            {pokemonNameOptions.map((pokemon, index) =>
-                loading? <OptionLoader/> : <Option onClick={()=>handleClickOption(pokemon)} key={index}><span>{pokemon?.name}</span></Option>
-            )}
+                {(!pokemonNameOptions || loading)? 
+                    <><OptionLoader/><OptionLoader/><OptionLoader/><OptionLoader/><OptionLoader/></> 
+                    :
+                    pokemonNameOptions.map((pokemon, index) =>
+                        <Option onClick={()=>handleClickOption(pokemon)} key={index}><span>{pokemon?.name}</span></Option>)}
            </OptionsContainer>
 }
